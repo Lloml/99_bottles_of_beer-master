5e53664d71245919aaa549ebb66379833650641a
DarkDeepFa <474823637@qq.com>
2018-06-11 17:39:09 +0800

    代码完成

---
+++ song.js
@@ -0,0 +1,20 @@
+function main(){
+    const sum = 99
+    for(let i = sum ;i>=0;i--){
+        if(i>1){
+            console.log(i+' bottles of beer on the wall, '+i+' bottles of beer.\n');
+            console.log('Take one down, pass it around, '+(i-1)+' bottles of beer on the wall.\n');
+        }
+        else if(i == 1){
+            console.log('1 bottle of beer on the wall, 1 bottle of beer.\n'+
+        'Take one down and pass it around, no more bottles of beer on the wall.\n')
+
+        }
+        else{
+            console.log('No more bottles of beer on the wall, no more bottles of beer.\n'+
+            'Go to the store and buy some more, 99 bottles of beer on the wall.\n')
+        }
+
+    }
+}
+main();
